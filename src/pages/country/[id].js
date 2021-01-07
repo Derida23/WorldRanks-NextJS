dc466b72e8b5bd0b78d3fe7ba10fa26fd53eb3e3
Arian Derida <arianderida@gmail.com>
2021-01-07 16:25:59 +0700

    handle layouting country page

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -1,7 +1,31 @@
 import styles from "./country.module.css";
 import Layout from "../../components/Layout/Layout";
+import { useEffect, useState } from "react";
+
+const getCountry = async (id) => {
+  const res = await fetch(`https://restcountries.eu/rest/v2/alpha/${id}`);
+
+  const country = await res.json();
+
+  return country;
+};
+
 const Country = ({ country }) => {
-  console.log(country);
+  const [borders, setBorders] = useState([]);
+
+  const getBorders = async () => {
+    const borders = await Promise.all(
+      country.borders.map((border) => getCountry(border))
+    );
+
+    setBorders(borders);
+  };
+
+  useEffect(() => {
+    getBorders();
+  }, []);
+
+  console.log(borders);
 
   return (
     <Layout title={country.name}>
@@ -24,6 +48,56 @@ const Country = ({ country }) => {
             </div>
           </div>
         </div>
+
+        <div className={styles.details_panel}>
+          <h4 className={styles.details_panel_heading}>Details</h4>
+
+          <div className={styles.details_panel_row}>
+            <div className={styles.details_panel_label}>Capital</div>
+            <div className={styles.details_panel_value}>{country.capital}</div>
+          </div>
+
+          <div className={styles.details_panel_row}>
+            <div className={styles.details_panel_label}>Languages</div>
+            <div className={styles.details_panel_value}>
+              {country.languages.map(({ name }) => name).join(", ")}
+            </div>
+          </div>
+
+          <div className={styles.details_panel_row}>
+            <div className={styles.details_panel_label}>Currencies</div>
+            <div className={styles.details_panel_value}>
+              {country.currencies.map(({ name }) => name).join(", ")}
+            </div>
+          </div>
+
+          <div className={styles.details_panel_row}>
+            <div className={styles.details_panel_label}>Native name</div>
+            <div className={styles.details_panel_value}>
+              {country.nativeName}
+            </div>
+          </div>
+
+          <div className={styles.details_panel_row}>
+            <div className={styles.details_panel_label}>Gini</div>
+            <div className={styles.details_panel_value}>{country.gini} %</div>
+          </div>
+
+          <div className={styles.details_panel_borders}>
+            <div className={styles.details_panel_borders_label}>
+              Neighbouring Countries
+            </div>
+            <div className={styles.details_panel_borders_container}>
+              {borders.map(({ flag, name }) => (
+                <div className={styles.details_panel_border_country}>
+                  <img src={flag} alt={name}></img>
+
+                  <div className={styles.details_panel_border_name}>{name}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </Layout>
   );
@@ -32,11 +106,7 @@ const Country = ({ country }) => {
 export default Country;
 
 export const getServerSideProps = async ({ params }) => {
-  const res = await fetch(
-    `https://restcountries.eu/rest/v2/alpha/${params.id}`
-  );
-
-  const country = await res.json();
+  const country = await getCountry(params.id);
 
   return {
     props: { country },
