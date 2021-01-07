0f1daf093122102ec5b4a5b6023c3a5f2de0a261
Arian Derida <arianderida@gmail.com>
2021-01-07 14:32:38 +0700

    handle filter region, sub region, and name

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,35 @@ import Layout from "../components/Layout/Layout";
 import styles from "../styles/Home.module.css";
 import SearchInput from "../components/SearchInput/SearchInput";
 import CountriesTable from "../components/CountriesTable/CountriesTable";
+import { useState } from "react";
+
 export default function Home({ countries }) {
   // console.log(countries);
+
+  const [keyword, setKeyword] = useState();
+
+  const filteredCountries = countries.filter(
+    (country) =>
+      country.name.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword) ||
+      country.subregion.toLowerCase().includes(keyword)
+  );
+  const onInputChange = (e) => {
+    e.preventDefault();
+
+    setKeyword(e.target.value.toLowerCase());
+  };
+
   return (
     <Layout>
       <div className={styles.counts}>Found {countries.length} countries</div>
 
-      <SearchInput placeholder="Filter by Name, Region or Sub-Region" />
+      <SearchInput
+        placeholder="Filter by Name, Region or Sub-Region"
+        onChange={onInputChange}
+      />
 
-      <CountriesTable countries={countries} />
+      <CountriesTable countries={filteredCountries} />
     </Layout>
   );
 }
