96f24bda5022465c700b0806a0f57aed29ce34d9
Arian Derida <arianderida@gmail.com>
2021-01-07 11:37:44 +0700

    handle create table css

--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -1,7 +1,12 @@
-const SearchInput = () => {
+import { SearchOutlined } from "@ant-design/icons";
+import styles from "./SearchInput.module.css";
+const SearchInput = ({ ...rest }) => {
   return (
-    <div>
-      <input />
+    <div className={styles.wrapper}>
+      <SearchOutlined color="inherit" />
+      <input className={styles.input} {...rest} />
     </div>
   );
 };
+
+export default SearchInput;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
 import Head from "next/head";
 import Layout from "../components/Layout/Layout";
 import styles from "../styles/Home.module.css";
-
+import SearchInput from "../components/SearchInput/SearchInput";
+import CountriesTable from "../components/CountriesTable/CountriesTable";
 export default function Home({ countries }) {
-  console.log(countries);
+  // console.log(countries);
   return (
     <Layout>
       <div className={styles.counts}>Found {countries.length} countries</div>
-      {/* <SearchInput /> */}
+
+      <SearchInput placeholder="Filter by Name, Region or Sub-Region" />
+
+      <CountriesTable countries={countries} />
     </Layout>
   );
 }
