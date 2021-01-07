e96c4861b3399433ef43f6716d1af7870591207d
Arian Derida <arianderida@gmail.com>
2021-01-07 15:21:41 +0700

    handle flag decoration css

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -1,7 +1,32 @@
+import styles from "./country.module.css";
+import Layout from "../../components/Layout/Layout";
 const Country = ({ country }) => {
   console.log(country);
 
-  return <div>Country</div>;
+  return (
+    <Layout title={country.name}>
+      <div>
+        <div className={styles.overview_panel}>
+          <img src={country.flag} alt={country.name} />
+
+          <h1 className={styles.overview_name}>{country.name}</h1>
+          <div className={styles.overview_region}>{country.region}</div>
+
+          <div className={styles.overview_numbers}>
+            <div className={styles.overview_population}>
+              <div className={styles.overview_value}>{country.population}</div>
+              <div className={styles.overview_label}>Population</div>
+            </div>
+
+            <div className={styles.overview_area}>
+              <div className={styles.overview_value}>{country.area}</div>
+              <div className={styles.overview_label}>Area</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Country;
