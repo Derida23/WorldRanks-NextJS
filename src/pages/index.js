10744b458d304ab7a803c6741a5edd878bc5e24e
Arian Derida <arianderida@gmail.com>
2021-01-19 14:39:25 +0700

    handle responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,13 +24,16 @@ export default function Home({ countries }) {
 
   return (
     <Layout>
-      <div className={styles.counts}>Found {countries.length} countries</div>
+      <div className={styles.inputContainer}>
+        <div className={styles.counts}>Found {countries.length} countries</div>
 
-      <SearchInput
-        placeholder="Filter by Name, Region or Sub-Region"
-        onChange={onInputChange}
-      />
-
+        <div className={styles.input}>
+          <SearchInput
+            placeholder="Filter by Name, Region or Sub-Region"
+            onChange={onInputChange}
+          />
+        </div>
+      </div>
       <CountriesTable countries={filteredCountries} />
     </Layout>
   );
